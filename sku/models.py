c18e7558dad16a8cf413eb3f4354173ed3514eb3
fall4u <fall4ufeng@gmail.com>
2018-01-01 18:48:10 +0800

    use ajax POST to get datatable's data

--- sku/models.py
+++ sku/models.py
@@ -8,3 +8,24 @@ from django.db import models
 
 class Document(models.Model):
     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=48)
+    author = models.CharField(max_length=24)
+    isbn = models.IntegerField()
+    status = models.CharField(max_length=12)
+    press = models.CharField(max_length=48)
+    acount = models.IntegerField()
+    lendCount = models.IntegerField()
+
+    def as_dict(self):
+    	return {
+    		"name": self.name,
+    		"author": self.author,
+    		"isbn" : self.isbn,
+    		"status" : self.status,
+    		"press" : self.press,
+    		"acount" : self.acount,
+    		"lendCount" : self.lendCount
+    	}
